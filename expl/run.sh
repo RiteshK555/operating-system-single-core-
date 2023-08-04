33280eb613976078c4040ace374fcab837516946
RiteshK555 <riteshkatukojwala@gmail.com>
2023-08-04 20:02:33 +0530

    operating sys full

---
+++ expl/run.sh
@@ -0,0 +1,59 @@
+echo "# even";
+./expl ./progs/even.expl
+
+echo "# idle";
+./expl ./progs/idle.expl
+
+echo "# multiply";
+./expl ./progs/multiply.expl
+
+echo "# num_50";
+./expl ./progs/num_50.expl
+
+echo "# odd";
+./expl ./progs/odd.expl
+
+echo "# squares";
+./expl ./progs/squares.expl
+
+echo "# load exec";
+./expl ./progs/load_exec.expl
+
+echo "# fork call example";
+./expl ./progs/fork_example.expl
+
+echo "# shell program";
+./expl ./progs/shell_program.expl
+
+echo "# reader writer program";
+./expl ./progs/r_w.expl
+
+echo "# linked list";
+./expl ./progs/linked_list.expl
+
+echo "# stage 23";
+./expl ./progs/stage_23.expl
+
+echo "# cat";
+./expl ./progs/cat.expl
+
+echo "# ls";
+./expl ./progs/ls.expl;
+
+echo "# rm";
+./expl ./progs/rm.expl;
+
+echo "# cp";
+./expl ./progs/cp.expl;
+
+echo "# login";
+./expl ./progs/login.expl;
+
+echo "# list all users"
+./expl ./progs/lu.expl;
+
+echo "# remove user files"
+./expl ./progs/rm.expl;
+
+echo "# stage 27"
+./expl ./progs/stage_27.expl;
---
+++ spl/run.sh
@@ -0,0 +1,62 @@
+echo "# boot module";
+./spl ./progs/boot_module.spl
+echo "# context switch";
+./spl ./progs/context_switch_module.spl
+echo "# halt prog";
+./spl ./progs/haltprog.spl
+echo "# int 7 handler with module";
+./spl ./progs/int7_handler_with_module.spl
+echo "# multi prog timer";
+./spl ./progs/multiprog_timer.spl
+echo "# os startup with boot module";
+./spl ./progs/os_startup_with_boot_module.spl
+echo "# device manager module";
+./spl ./progs/device_manager_module.spl
+echo "# console interrupt handler";
+./spl ./progs/console_interrupt_handler.spl
+echo "# oddnos";
+./spl ./progs/oddnos.spl
+echo "# os startup";
+./spl ./progs/os_startup.spl
+echo "# timer with context switch";
+./spl ./progs/timer_with_context_switch_module.spl
+echo "# exception hanlder";
+./spl ./progs/exception_handler.spl
+echo "# int 6 handler";
+./spl ./progs/int6_handler.spl
+echo "# int7 handler";
+./spl ./progs/interrupt_7_handler.spl
+echo "# resource manager module";
+./spl ./progs/resources_manager_module.spl
+echo "# interrupt 9 handler";
+./spl ./progs/int9_handler.spl
+echo "# process manager module";
+./spl ./progs/process_manager_module.spl
+echo "# memory manager module";
+./spl ./progs/memory_manager_module.spl
+echo "# disk interrupt handler";
+./spl ./progs/disk_interrupt_handler.spl
+echo "# fork sys call";
+./spl ./progs/fork_system_call.spl
+echo "# int 11 handler";
+./spl ./progs/int11_handler.spl
+echo "# int 15 handler";
+./spl ./progs/int15_handler.spl
+echo "# int 13 handler";
+./spl ./progs/int13_handler.spl
+echo "# int 14 handler";
+./spl ./progs/int14_handler.spl
+echo "# int 4 handler";
+./spl ./progs/int4_handler.spl
+echo "# int 5 handler";
+./spl ./progs/int5_handler.spl
+echo "# file manager module";
+./spl ./progs/file_manager_module.spl
+echo "# int 17 handler";
+./spl ./progs/int17_handler.spl
+echo "# int 12 handler";
+./spl ./progs/logout.spl
+echo "# int 16 handler";
+./spl ./progs/int16_handler.spl
+echo "# pager module";
+./spl ./progs/pager_module.spl
